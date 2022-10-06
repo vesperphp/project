a3ece43190ac100d091c9d370351665474c85297
Rik Janssen <56626554+rik-janssen@users.noreply.github.com>
2022-10-06 22:26:51 +0200

    Small changes

--- init/config.php
+++ init/config.php
@@ -1,16 +1,38 @@
 <?php
-
 $config = [
+
+  /**
+   * The overall website
+   * settings for your
+   * project.
+   */
+
   'site' => [
       'title' => 'Vesper-PHP',
       'description' => 'The ultimate rag tag PHP framework.',
       'uri' => 'http://localhost:8888/Vesper-PHP/httpdocs'
     ],
 
-  'production' => false, // Production: true / Development: false
-  'public' => 'httpdocs/', // Public folder directory
+  /** 
+   * Production or Live?
+   * true : live
+   * false : dev/prod
+   */
+
+  'production' => false, 
+
+  /**
+   * The url to redirect some
+   * of the errors to.
+   */
+
   'errordirect' => 'error/500/', // Location to redirect errors to.
   
+  /**
+   * MySQL settings.
+   * (for Sequel)
+   */
+
   'mysql' => [ 
     'host' => '127.0.0.1',
     'db' => 'test',
@@ -18,7 +40,22 @@ $config = [
     'pass' => 'root',
     'port' => '8889'
     ],
+
+  /**
+   * Sequel settings.
+   */
+
+  'sequel' => [
+    'paginate' => 10,      // Items per loop
+    'limit' =>  25,        // standard limit (when applied)
+    'debug' => false,      // Show the instance internals
+  ],
   
+  /**
+   * SMTP settings.
+   * (for PHP mailer)
+   */
+
   'smtp' => [
     'host' => 'smtp.mailtrap.io',
     'user' => '',
@@ -27,10 +64,23 @@ $config = [
     'encrypt' => 'STARTTLS'
   ],
 
+  /**
+   * Template engine settings.
+   * (for Frontier)
+   */
+
   'frontier' => [
-    'cache' => 'httpdocs/cache/', // cached templates folder
-    'views' => 'views/', // template view folder
-    'namespaces' => [ // use namespace; on cached template
+    'cache' => 'httpdocs/cache/',
+    'views' => 'views/', 
+
+    /**
+     * These are the namespaces
+     * that are placed on top of
+     * every cached template so the
+     * classes can be used.
+     */
+
+    'namespaces' => [ 
       'Elemental\Config',
       'Elemental\Re',
       'Frontier\Glob',
@@ -38,14 +88,33 @@ $config = [
       'Frontier\Hook',
       'Sequel\Paginate'
     ],
+    
+    /**
+     * Flash message settings.
+     */
+
     'flash' => [
       'template' => 'views/parts/flash.html' // the template file for a flash message
     ]
   ],
 
-  'hash' => [
+  /**
+   * Security settings.
+   * (for Vault)
+   */
+
+  'vault' => [
     'salt' => '47f!hX4AB6H31l~rIQHH3ESyu$R6A6iCMk2XXSfEBOX7dMw4E%g9Ekes5Uxz',
   ],
+
+  /**
+   * Logger settings.
+   * Toggle on/of what you'd
+   * like to log. Mostly for
+   * debugging but some
+   * of them can be useful 
+   * on a live site.
+   */
 
   'logger' => [
     'phperror' => true,    // log php errors
@@ -57,15 +126,12 @@ $config = [
     'visits' => false,     // log visits. Warning, this has to reflect in your cookie notice
     //'sendry' => false,   // log sendry errors
     //'sent' => false,     // log sent emails
-  ],
-
-  'sequel' => [
-    'paginate' => 10,      // Items per loop
-    'limit' =>  25,        // standard limit (when applied)
-    'debug' => false,      // Show the instance internals
   ]
-
   
 ];
+
+/**
+ * Done!
+ */
 
 define("V_CONFIG", $config);
